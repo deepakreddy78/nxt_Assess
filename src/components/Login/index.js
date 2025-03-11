173b220a83610910f93b17e13a9b48fd1c94a69f
deepakreddy78 <deepakreddy78@gmail.com>
2025-03-11 15:46:42 +0530

    Initial commit

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,7 @@
 import {Component} from 'react'
+import Cookies from 'js-cookie'
 
+import logo from './imgs/logo.svg'
 import './index.css'
 
 class Login extends Component {
@@ -7,6 +9,8 @@ class Login extends Component {
     username: '',
     password: '',
     checkBox: false,
+    showSubmitError: false,
+    errorMsg: '',
   }
 
   onChangeUserValue = event => {
@@ -19,6 +23,34 @@ class Login extends Component {
 
   onChangeCheckBoxValue = () => {
     this.setState(preVal => ({checkBox: !preVal.checkBox}))
+  }
+
+  onSubmitSuccess = token => {
+    const {history} = this.props
+    Cookies.set('jwt_token', token, {expires: 30})
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({showSubmitError: true, errorMsg})
+  }
+
+  handelformSubmit = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const URL = 'https://apis.ccbp.in/login'
+    const option = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(URL, option)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
   }
 
   renderUserName = () => {
@@ -71,18 +103,20 @@ class Login extends Component {
   )
 
   render() {
+    const {errorMsg, showSubmitError} = this.state
     return (
       <div className="login-container">
         <div className="login-form">
-          <img
-            src="https://www.figma.com/347c1cda-0813-45d0-9af7-da78084fb008"
-            alt="logo"
-          />
-
-          {this.renderUserName()}
-          {this.renderPassword()}
-          {this.renderCheckBox()}
-          <button type="button">Submit</button>
+          <img src={logo} className="nxt-logo" alt="logo" />
+          <form onSubmit={this.handelformSubmit}>
+            {this.renderUserName()}
+            {this.renderPassword()}
+            {this.renderCheckBox()}
+            <button className="submit-button" type="submit">
+              Submit
+            </button>
+            {showSubmitError && <p className="error-msg">{errorMsg}</p>}
+          </form>
         </div>
       </div>
     )
